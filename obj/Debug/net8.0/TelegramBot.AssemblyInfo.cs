088e3b758bf5ed7e17b5a44d3d7609e683287631
Urii Lutsenko <dlyawarfeisa@gmail.com>
2024-08-12 15:07:22 +0300

    change net

--- obj/Debug/net8.0/TelegramBot.AssemblyInfo.cs
+++ obj/Debug/net8.0/TelegramBot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TelegramBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b4a67013abf470a779c4b176c5c7b27cbe5f6f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd64fd3b8977583659a398918565fb8783784351")]
 [assembly: System.Reflection.AssemblyProductAttribute("TelegramBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TelegramBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
