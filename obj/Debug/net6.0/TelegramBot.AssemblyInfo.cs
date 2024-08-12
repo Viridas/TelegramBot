fbd429d2838ccc6a111f8d614b0c1e3c8348ba04
Urii Lutsenko <dlyawarfeisa@gmail.com>
2024-08-12 13:58:27 +0300

    ch proc

--- obj/Debug/net6.0/TelegramBot.AssemblyInfo.cs
+++ obj/Debug/net6.0/TelegramBot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TelegramBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abea13bdbcc0a008b1ded46e965205200908804a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af772baa407002bcfcdf555ef02e1c55e1529a5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TelegramBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TelegramBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
